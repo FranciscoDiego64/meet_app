071ba8a43a631f9a1d299e6ae016912974d34cd7
FranciscoDiego64 <112688191+FranciscoDiego64@users.noreply.github.com>
2023-01-20 12:40:48 -0600

    Fixed critical erros with axios, downgraded puppeteer and mock data file

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -2,7 +2,7 @@ import { loadFeature, defineFeature } from 'jest-cucumber';
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import App from '../App';
-import { mockData } from '../mockData';
+import { mockData } from '../mock-data';
 import CitySearch from '../CitySearch';
 import { extractLocations } from '../api';
 
